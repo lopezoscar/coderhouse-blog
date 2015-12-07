686119bc4eacad08673d750680208b0661920e58
Oscar López <olopez@despegar.com>
2015-12-06 21:04:51 -0300

    se agrega vhost

--- app.js
+++ app.js
@@ -1,45 +1,70 @@
 "use strict";
 
-//Load Libraries
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
-var mongojs = require("mongojs");
 
-//Create db instance
-var CONFIG = require("./db");
-var db = mongojs("mongodb://"+CONFIG.host+":"+CONFIG.port+"/"+CONFIG.db,CONFIG.collections);
 
 //Load APP Libraries
 global.Blog = require("./lib/Blog")(db);
 global.Users = require("./lib/Users")(db);
 global.Posts = require("./lib/Posts")(db);
 
-//Set template engine
-app.set('view engine','handlebars');
-app.engine('html',exphbs());
+//Load Libraries
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var mongojs = require("mongojs");
+var vhost = require('express-vhost');
 
-//Set static folders
-app.use(express.static('templates'));
-app.use(express.static('public/default'));
+//Create db instance
+var CONFIG = require("./db");
+var db = mongojs("mongodb://"+CONFIG.host+":"+CONFIG.port+"/"+CONFIG.db,CONFIG.collections);
 
-//Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+function buildAPP(){
+    //Set template engine
+    app.set('view engine','handlebars');
+    app.engine('html',exphbs());
 
-//Routing Endpoints
-app.use("/posts",require("./api/posts"));
-app.use("/users",require("./api/users"));
-app.use("/blog" ,require("./api/blog"));
+    //Set static folders
+    app.use(express.static('templates'));
+    app.use(express.static('public/default'));
 
-//One Page only
-app.get('/',function(req,res){
-    res.render('default/index.html',{layout:false});
-});
+    //Body Parser
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    //Routing Endpoints
+    app.use("/posts",require("./api/posts"));
+    app.use("/users",require("./api/users"));
+    app.use("/blog" ,require("./api/blog"));
+
+    //One Page only
+    app.get('/',function(req,res){
+        res.render('default/index.html',{layout:false});
+    });
+
+    return app;
+}
+
+//VHOST Logic
+var server = express();
+server.use(vhost.vhost(server.enabled('trust proxy')));
 
 //Start Server on port
-app.listen(process.argv[2],function(){
+server.listen(process.argv[2],function(){
     console.log("SERVER UP");
 });
+
+vhost.register("blog.com", buildAPP());
+
+db.blogs.find({},{domain:1},function(err,blogs){
+    if(err){
+        console.log("DOMAINS NOT FOUND");
+    }else{
+        for(var i = 0; i < blogs.length; i++){
+            vhost.register(blogs[i].domain, buildAPP());
+        }
+    }
+});
+
+
 
